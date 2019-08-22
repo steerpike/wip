ae8599e1ed07d5760267575dab6616ec4ed46b3d
Sam <drop.table.people@googlemail.com>
2019-08-22 16:02:23 +1000

    Quill added to document

--- src/Documents/Document.js
+++ src/Documents/Document.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { AuthContext } from '../AuthContext'
+import ReactQuill from 'react-quill';
+
+import 'react-quill/dist/quill.core.css';
+import 'react-quill/dist/quill.bubble.css'; 
 
 export default class Document extends React.Component {
     static contextType = AuthContext
     state = {
-        document: {}
+        document: { 
+            title: '', 
+            content: '' 
+        }
     }
     async componentDidMount() {
         const db = this.context.db;
@@ -17,14 +24,33 @@ export default class Document extends React.Component {
             })
         }
     }
+    updateTitle = (event) => {
+        let newTitle = event.target.value
+        this.setState(prevState => ({document: {
+            ...prevState.document,
+            title:newTitle
+        }}));
+    }
+    updateContent = (content) => {
+        let newContent = content
+        this.setState(prevState => ({document: {
+            ...prevState.document,
+            content:newContent
+        }}));
+    }
     render() {
         const { document } = this.state
         if(!document) { return null;}
         return (
             <div>
                 <h4>{document.createdAt}</h4>
-                <input type="text" name="title" onChange={this.updateValue} />
-                <textarea type="text" name="content" value={document.content} onChange={this.updateValue} />
+                <input type="text" name="title" defaultValue={document.title} onChange={this.updateTitle} />
+                <ReactQuill
+                    className="min-h-screen border reactQuill"
+                    theme="bubble"
+                    value={document.content}
+                    onChange={this.updateContent}
+                />
             </div>
         )
     }

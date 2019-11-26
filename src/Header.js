966403e94c52d3f39a5241ecfeb9a01dc122ba4a
Steerpike <drop.table.people@googlemail.com>
2019-11-26 20:31:35 +1100

    New menu header added

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,19 @@ import { Link } from 'react-router-dom';
 
 export default () => (
     <header>
+        <nav className="flex items-center justify-between px-4 py-2"> 
+            <div className="leading-none">
+                <span className="uppercase text-sm">Words<br /> in<br /> Progress<br /></span>
+            </div>
+            <div>
+                <button className="block">
+                    <svg className="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path fillRule="evenodd" clipRule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z" />
+                        <path fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" />
+                    </svg>
+                </button>
+            </div>
+        </nav>
         <nav className="flex items-center bg-gray-200">
         <AuthConsumer>
             {({ isAuth, logout, user, isAnonymous }) => (
